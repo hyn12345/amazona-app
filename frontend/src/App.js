5ecdfe306ea3a076457e8feda18abe7a99527bb7
Yining Hu <hyn@Yinings-MacBook-Pro.local>
2021-08-16 16:08:35 -0300

    commit

---
+++ frontend/src/App.js
@@ -0,0 +1,42 @@
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+function App() {
+
+const cart = useSelector((state) => state.cart);
+const { cartItems } = cart;
+
+  return (
+    <BrowserRouter>
+    <div className="grid-container">
+        <header className="row">
+            <div>
+                <Link className="brand" to="/">Amazona</Link>
+            </div>
+            <div>
+                <Link to="/cart/">
+                  Cart
+                  {cartItems.length>0 && (
+                    <span className="badge">{cartItems.length}</span>
+                  )}
+                </Link>
+                <Link to="signin.html">Sign In</Link>
+            </div>
+        </header>
+        <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">
+            All rights reserved.
+        </footer>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
